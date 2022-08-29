dacfed613279b8d8f33906759486e7eada4edf8e
Rustem Gimadiev <hrustim25@gmail.com>
2022-08-29 20:49:46 +0300

    Added msg codes, moved msgs to constants, fixed bugs

--- backend/webserver/methods.py
+++ backend/webserver/methods.py
@@ -2,6 +2,7 @@ import json
 from flask import abort, Response
 from .models import User, Room
 from . import db
+from .constants import *
 
 
 def is_json(data):
@@ -16,30 +17,43 @@ def load_json(data):
     return json.loads(data)
 
 
-def my_abort(msg):
-    abort(
-        Response(
-            response=json.dumps({'msg': msg}),
-            status=500,
-            headers={'Content-Type': 'application/json'},
+def my_abort(msg, err_code=None):
+    if err_code is None:
+        abort(
+            Response(
+                response=json.dumps({'msg': msg}),
+                status=500,
+                headers={'Content-Type': 'application/json'},
+            )
         )
-    )
+    else:
+        abort(
+            Response(
+                response=json.dumps({'msg': msg, 'code': err_code}),
+                status=500,
+                headers={'Content-Type': 'application/json'},
+            )
+        )
 
 
 def check_user(login, password):
     data = db.session.query(User).filter(User.login == login).all()
     if len(data) == 0:
-        return (False, 'User not found')
+        return (False, MSG_ERR_USER_NOT_FOUND, CODE_ERR_USER_NOT_FOUND)
     if not data[0].check_password(password):
-        return (False, 'Wrong password')
-    return (True, 'Success')
+        return (False, MSG_ERR_WRONG_PASSWORD, CODE_ERR_WRONG_PASSWORD)
+    return (True, )
 
 
-def get_jsonified_response(msg: str, **kwargs):
-    if len(kwargs) == 0:
-        return json.dumps({'msg': msg})
-    else:
-        return json.dumps({'msg': msg, 'data': kwargs})
+def get_jsonified_response(msg: str, code=None, **kwargs):
+    result = {'msg': msg}
+    if code is not None:
+        result['code'] = code
+    if 'data' in kwargs and len(kwargs) == 1:
+        result['data'] = kwargs['data']
+    elif len(kwargs) != 0:
+        result['data'] = kwargs
+    return json.dumps(result)
 
 
 def delete_empty_room(room_id):
--- backend/webserver/routes.py
+++ backend/webserver/routes.py
@@ -2,7 +2,9 @@ from flask import Blueprint, request, abort
 
 from webserver import db
 from webserver.models import User
-from .methods import my_abort, check_user
+from .methods import my_abort, check_user, get_jsonified_response
+
+from .constants import *
 
 bp = Blueprint("server", __name__)
 
@@ -13,14 +15,14 @@ def register():
         abort(404)
 
     if not request.is_json:
-        my_abort('Content-Type must be json!')
+        my_abort(MSG_ERR_WRONG_CONTENT_TYPE, CODE_ERR_WRONG_CONTENT_TYPE)
     data = request.json
     if 'login' not in data or 'password' not in data:
-        my_abort('No login or password')
+        my_abort(MSG_ERR_NO_LOGIN_OR_PASSWORD, CODE_ERR_NO_LOGIN_OR_PASSWORD)
 
     res = db.session.query(User).filter(User.login == data['login']).all()
     if len(res) > 0:
-        my_abort('User already exists')
+        my_abort(MSG_ERR_USER_ALREADY_EXISTS, CODE_ERR_USER_ALREADY_EXISTS)
 
     new_user = User(login=data['login'])
     new_user.set_password(data['password'])
@@ -30,7 +32,7 @@ def register():
     user_data = dict(db.session.query(User.id, User.coins).filter(
         User.login == data['login']).first())
 
-    return {'msg': 'User register successful', 'data': user_data}
+    return get_jsonified_response(MSG_REGISTER_SUCCESSFUL, CODE_REGISTER_SUCCESSFUL, data=user_data)
 
 
 @bp.route('/login', methods=['GET', 'POST'])
@@ -39,15 +41,15 @@ def login():
         abort(404)
 
     if not request.is_json:
-        my_abort('Content-Type must be json!')
+        my_abort(MSG_ERR_WRONG_CONTENT_TYPE, CODE_ERR_WRONG_CONTENT_TYPE)
     data = request.json
     if 'login' not in data or 'password' not in data:
-        my_abort('No login or password')
+        my_abort(MSG_ERR_NO_LOGIN_OR_PASSWORD, CODE_ERR_NO_LOGIN_OR_PASSWORD)
 
     check_result = check_user(data['login'], data['password'])
     if not check_result[0]:
-        my_abort(check_result[1])
+        my_abort(check_result[1], check_result[2])
 
     user_data = dict(db.session.query(User.id, User.coins).filter(
         User.login == data['login']).first())
-    return {'msg': 'User login successful', 'data': user_data}
+    return get_jsonified_response(MSG_LOGIN_SUCCESSFUL, CODE_LOGIN_SUCCESSFUL, data=user_data)
